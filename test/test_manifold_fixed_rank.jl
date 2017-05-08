4a5e8a906652e638ffc5a267c0d9d5cbc42cfdc9
martinjm97 <jessem.michel@gmail.com>
2017-05-08 15:35:43 -0400

    outlines for fixed rank and stiefel testing

---
+++ test/test_manifold_fixed_rank.jl
@@ -0,0 +1,181 @@
+m = 10
+n = 5
+k = 3
+s = JManOpt.FixedRank(m, n, k)
+
+@test JManOpt.dim(s) == (m + n - k) * k
+
+@test JManOpt.dim(s) == JManOpt.typicaldist(s)
+
+a = rand(s)
+x = JManOpt.randvec(s, a)
+y = JManOpt.randvec(s, a)
+
+
+x = rand(s)
+a = JManOpt.randvec(s,x)
+b = JManOpt.randvec(s,x)
+# First embed in the ambient space
+A = dot(dot(a[2], x[3]), x[1]) + dot(x[3], a[1]) + dot(a[3]', x[1])
+B = dot(x[1], dot(b[2], x[3])) + dot(b[1], x[3]) + dot(x[1], b[3]')
+trueinner = sum(A * B)
+@test isapprox(trueinner, JManOpt.inner(s, x, a, b))
+
+x = rand(s)
+v = randn(m, n)
+
+g = JManOpt.proj(s, x, v)
+# Check that g is a true tangent vector
+@test isapprox(dot(g[1]', x[1]), zeros(k, k),atol=1e-6)
+@test isapprox(dot(g[3]', x[3]'), zeros(k, k), atol=1e-6)
+
+# Verify that proj gives the closest point within the tangent space
+# by displacing the result slightly and checking that this increases
+# the distance.
+x = rand(s)
+v = randn(m, n)
+
+g = JManOpt.proj(x, v)
+# Displace g a little
+g_disp = g + 0.01 * JManOpt.randvec(x)
+
+# Return to the ambient representation
+g = JManOpt.tangent2ambient(x, g)
+g_disp = JManOpt.tangent2ambient(x, g_disp)
+g = dot(g[3]', dot(g[1], g[2]))
+g_disp = dot(g_disp[3]', dot(g_disp[2], g_disp[1]))
+
+@test norm(g - v) < norm(g_disp - v)
+
+# Verify that proj leaves tangent vectors unchanged
+x = rand(s)
+u = JManOpt.randvec(s,x)
+A = JManOpt.proj(x, JManOpt.tangent2ambient(x, u))
+B = u
+# diff = [A[k]-B[k] for k in xrange(len(A))]
+@test isapprox(A[1], B[1])
+@test isapprox(A[2], B[2])
+@test isapprox(A[3], B[3])
+
+x = rand(s)
+u = JManOpt.randvec(s,x)
+@test isapprox(sqrt(JManOpt.inner(x, u, u)), norm(x, u))
+
+x = rand(s)
+y = rand(s)
+@test shape(x[1]) == (m, k)
+@test shape(x[2]) == (k,)
+@test shape(x[3]) == (k, n)
+@test isapprox(dot(x[1], x[1]'), eye(k), atol=1e-6)
+@test isapprox(dot(x[3]', x[3]), eye(k), atol=1e-6)
+
+@test norm(x[0] - y[0]) > 1e-6
+@test norm(x[1] - y[1]) > 1e-6
+@test norm(x[2] - y[2]) > 1e-6
+
+x = rand(s)
+y = rand(s)
+u = JManOpt.randvec(x)
+A = JManOpt.transp(x, y, u)
+B = JManOpt.proj(y, JManOpt.tangent2ambient(x, u))
+diff = [A[k]-B[k] for k in range(len(A))]
+@test isapprox(JManOpt.norm(y, diff), 0)
+
+
+z = randn(m, n)
+
+# Set u, s, v so that z = u.dot(s).dot(v.T)
+u, s, v = svd(z)
+s = diag(s)
+v = v'
+
+w = randn(n, n)
+
+@test isapprox(dot(w, z), JManOpt._apply_ambient(z, w))
+@test isapprox(dot(w, z), JManOpt._apply_ambient((u, s, v), w))
+
+z = randn(n, m)
+
+# Set u, s, v so that z = u.dot(s).dot(v.T)
+u, s, v = svd(z)
+s = diag(s)
+v = v'
+
+w = randn(n, n)
+
+@test isapprox(dot(w, z'), JManOpt._apply_ambient_transpose(z, w))
+@test isapprox(dot(w, z'), JManOpt._apply_ambient_transpose((u, s, v), w))
+
+x = rand(s)
+z = JManOpt.randvec(s, x)
+
+z_ambient = (dot(x[3], dot(z[2], x[1])) + dot(x[3], z[1]) +
+             dot(z[3]', x[1]))
+
+u, s, v = JManOpt.tangent2ambient(x, z)
+
+@test isapprox(z_ambient, dot(v, dot(u, s)))
+
+# test_ehess2rhess()
+
+# Test that the result is on the manifold and that for small
+# tangent vectors it has little effect.
+x = rand(s)
+u = JManOpt.randvec(s, x)
+
+y = JManOpt.retr(s, x, u)
+
+@test isapprox(dot(y[1]', y[1]), eye(k), atol=1e-6)
+@test isapprox(dot(y[3] ,y[3]'), eye(k), atol=1e-6)
+
+u = u * 1e-6
+y = JManOpt.retr(s, x, u)
+y = y[1].dot(diag(y[2])).dot(y[3])
+
+u = JManOpt.tangent2ambient(x, u)
+u = dot(u[3]', dot(u[1], u[2]))
+x = dot(dot(x[1], diag(x[2])), x[3])
+
+@test isapprox(y, x + u, atol=1e-5)
+
+# TODO test egrad2rgrad
+# Verify that egrad2rgrad and proj are equivalent.
+# x = rand(s)
+# u, s, vt = x
+#
+# i = eye(k)
+#
+# f = 1 / (s[..., newaxis, :]**2 - s[..., :, newaxis]**2 + i)
+#
+# du = random.randn(m, k)
+# ds = random.randn(k)
+# dvt = random.randn(k, n)
+#
+# Up = (dot(dot(eye(m) - dot(u, u.T), du),
+#              linalg.inv(diag(s))))
+# M = (dot(f * (dot(u.T, du) - dot(du.T, u)), diag(s)) +
+#      dot(diag(s), f * (dot(vt, dvt.T) - dot(dvt, vt.T))) +
+#      diag(ds))
+# Vp = (dot(dot(eye(n) - dot(vt.T, vt), dvt.T),
+#              linalg.inv(diag(s))))
+#
+# up, m, vp = m.egrad2rgrad(x, (du, ds, dvt))
+#
+# @test isapprox(Up, up)
+# @test isapprox(M, m)
+# @test isapprox(Vp, vp)
+
+x = rand(s)
+u = JManOpt.randvec(s,x)
+
+# Check that u is a tangent vector
+@test size(u[1]) == (m, k)
+@test size(u[2]) == (k, k)
+@test size(u[3]) == (n, k)
+@test isapprox(dot(u[1]', x[1]), zeros(k, k), atol=1e-6)
+@test isapprox(dot(u[3]', x[3]'), zeros(k, k), atol=1e-6)
+
+v = JManOpt.randvec(s,x)
+
+@test isapprox(JManOpt.norm(x, u), 1)
+@test JManOpt.norm(x, u - v) > 1e-6
---
+++ test/test_manifold_stiefel.jl
@@ -0,0 +1,178 @@
+m = 20
+n = 2
+k = 1
+s = JManOpt.Stiefel(m, n, k)
+#self.proj = lambda x, u: u - npa.dot(x, npa.dot(x.T, u) +
+#                                     npa.dot(u.T, x)) / 2
+
+@test JManOpt.dim(s) == 0.5 * n* (2 * m- n- 1)
+
+#  test_typicaldist():
+
+#  test_dist(self):
+
+X = qr(randn(m, n))[1]
+A, B = randn(2, m, n)
+@test isapprox(sum(A * B), JManOpt.inner(s, X, A, B))
+
+
+# Construct a random point X on the manifold.
+X = randn(m, n)
+X = qr(X)[1]
+
+# Construct a vector H in the ambient space.
+H = randn(m, n)
+
+# Compare the projections.
+Hproj = H - dot(X,dot(H, X') + dot(X, H')) / 2
+@test isapprox(Hproj, JManOpt.proj(s,X, H))
+
+
+# Just make sure that things generated are on the manifold and that
+# if you generate two they are not equal.
+X = rand(s)
+@test isapprox(dot(X, X'), eye(n), atol=1e-10)
+Y = rand(s)
+@test vecnorm(X - Y) > 1e-6
+
+# Make sure things generated are in tangent space and if you generate
+# two then they are not equal.
+X = rand(s)
+U = randvec(s,X)
+@test isapprox(JManOpt.multisym(dot(U, X')), zeros(n, n), atol=1e-10)
+V = randvec(s,X)
+assert vecnorm(U - V) > 1e-6
+
+# Test that the result is on the manifold and that for small
+# tangent vectors it has little effect.
+x = rand(s)
+u = randvec(s, x)
+
+xretru = JManOpt.retr(s,x, u)
+@test isapprox(dot(xretru, xretru'), eye(n,n), atol=1e-10)
+
+u = u * 1e-6
+xretru = JManOpt.retr(s,x, u)
+@test isapprox(xretru, x + u)
+
+# Test this function at some randomly generated point.
+x = rand(s)
+u = randvec(s, x)
+egrad = randn(m, n)
+ehess = randn(m, n)
+
+#TODO learn how to test EHESS
+#@test isapprox(testing.ehess2rhess(self.proj)(x, egrad,
+#                                                          ehess, u),
+#                           man.ehess2rhess(x, egrad, ehess, u))
+
+
+#  test_egrad2rgrad():
+x = rand(s)
+u = randvec(s, x)
+@test isapprox(norm(s, x, u), vecnorm(u))
+
+#  test_transp(self):
+
+# Check that exp lies on the manifold and that exp of a small vector u
+# is close to x + u.
+s = man
+x = rand(s)
+u = randvec(s, x)
+
+xexpu = exp(s, x, u)
+@test isapprox(dot(xexpu, xexpu'), eye(n,n), atol=1e-10)
+
+u = u * 1e-6
+xexpu = exp(s, x, u)
+@test isapprox(xexpu, x + u)
+
+#  test_exp_log_inverse
+#  test_log_exp_inverse
+#  test_pairmean
+
+
+####################
+# Multidim Steifel #
+####################
+m = 10
+n = 3
+k = 3
+s = Stiefel(m, n, k)
+
+@test man.dim == 0.5 * k * n* (2 * m - n - 1)
+
+@test isapprox(typicaldist(s), sqrt(n * k))
+
+#  test_dist(self):
+
+X = rand(s)
+A = randvec(s,X)
+B = randvec(s,X)
+@test isapprox(sum(A .* B), JManOpt.inner(s, X, A, B))
+
+# Construct a random point X on the manifold.
+X = rand(s)
+
+# Construct a vector H in the ambient space.
+H = randn(k, m, n)
+
+# Compare the projections.
+Hproj = H - multiprod(X, multiprod(JManOpt.multitransp(X), H) +
+                      multiprod(JManOpt.multitransp(H), X)) / 2
+@test isapprox(Hproj, JManOpt.proj(s, X, H))
+
+# Just make sure that things generated are on the manifold and that
+# if you generate two they are not equal.
+X = rand(s)
+@test isapprox(multiprod(JManOpt.multitransp(X), X), multieye(k, n), atol=1e-10)
+Y = rand(s)
+@test vecnorm(X - Y) > 1e-6
+
+# Make sure things generated are in tangent space and if you generate
+# two then they are not equal.
+X = rand(s)
+U = randvec(s,X)
+@test isapprox(JManOpt.multisym(multiprod(JManOpt.multitransp(X), U)), zeros(k, n, n),atol=1e-10)
+V = randvec(s,X)
+@test vecnorm(U - V) > 1e-6
+
+# Test that the result is on the manifold and that for small
+# tangent vectors it has little effect.
+x = rand(s)
+u = randvec(s, x)
+
+xretru = retr(s, x, u)
+
+@test isapprox(multiprod(JManOpt.multitransp(xretru), xretru), multieye(s.k, n), atol=1e-10)
+
+u = u * 1e-6
+xretru = JManOpt.retr(s,x, u)
+@test isapprox(xretru, x + u)
+
+#  test_egrad2rgrad(self):
+
+x = rand(s)
+u = randvec(s, x)
+@test isapprox(norm(s, x, u), vecnorm(u))
+
+#  test_transp(self):
+
+# Check that exp lies on the manifold and that exp of a small vector u
+# is close to x + u.
+s = man
+x = rand(s)
+u = randvec(s, x)
+
+xexpu = exp(s, x, u)
+@test isapprox(multiprod(JManOpt.multitransp(xexpu), xexpu), multieye(k, n), atol=1e-10)
+
+u = u * 1e-6
+xexpu = exp(s, x, u)
+@test isapprox(xexpu, x + u)
+
+# test_exp_log_inverse
+
+# test_log_exp_inverse
+
+# test_pairmean
