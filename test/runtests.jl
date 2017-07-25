ba0fd2cacf1344a01708891f59116c347efdcec0
martinjm97 <jessem.michel@gmail.com>
2017-07-25 01:02:34 -0400

    Euclidean, Sphere, multi tested

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,7 +5,7 @@ using Base.Test
 include("test_manifold_euclidean.jl")
 include("test_tools_multi.jl")
 include("test_manifold_sphere.jl")
-include("test_manifold_posdef.jl")
+#include("test_manifold_posdef.jl")
 #include("test_manifold_oblique.jl")
 #include("test_manifold_grassman.jl")
 #include("test_manifold_stiefel.jl")
